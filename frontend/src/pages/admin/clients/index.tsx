d063b1cef584c8e3e7c44dcce4aa181b2ecb2811
BayuFaturahman <bayu.faturahman8@gmail.com>
2025-11-01 19:31:43 +0700

    update

--- frontend/src/pages/admin/clients/index.tsx
+++ frontend/src/pages/admin/clients/index.tsx
@@ -208,7 +208,7 @@ export default function ApiClientsPage() {
               <option value="hilogate">Hilogate</option>
               <option value="oy">OY Indonesia</option>
               <option value="gidi">Gidi</option>
-              <option value="ing1">INA Cash</option>
+              <option value="ing1">INA1</option>
             </select>
             <select
               value={newForceSchedule}
